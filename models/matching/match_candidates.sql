194b0232313d317edfa2e19d99e86d933111bb89
Holger Bruch <hb@mfdz.de>
2025-08-15 12:26:33 +0200

    fix: pre-filter match candidates

--- models/matching/match_candidates.sql
+++ models/matching/match_candidates.sql
@@ -7,14 +7,21 @@ MODEL (
   )
 );
 
-WITH distance_ranked_match_candidates AS (
+WITH filtered_match_candidates AS (
+  SELECT mv.* FROM matching.match_candidates_in_vicinity mv
+    JOIN stage.osm_stops o USING (osm_id)
+    JOIN matching.transit_stops t USING (globaleid)
+WHERE NOT ifnull((o.mode in ('trainish', 'train','light_rail','tram', 'ferry') AND t.mode IN ('bus'))
+OR (o.mode in ('bus') AND t.mode IN ('tram', 'light_rail', 'train', 'trainish', 'ferry')), false)
+),
+distance_ranked_match_candidates AS (
   SELECT
     *,
     ROW_NUMBER() OVER (PARTITION BY globaleId ORDER BY distance ASC) AS stop_ranking
-  FROM matching.match_candidates_in_vicinity
+  FROM filtered_match_candidates
 )
 SELECT
   *
 FROM distance_ranked_match_candidates
 WHERE
-  stop_ranking <= LEAST(number_of_station_quays + 5, 15)
+  stop_ranking <= LEAST(number_of_station_quays + 5, @MAX_NUMBER_OF_CANDIDATES_PER_QUAY)

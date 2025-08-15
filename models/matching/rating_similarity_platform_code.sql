9238f38ad1b0f7bf50934ed77c5729d0924d3211
Holger Bruch <hb@mfdz.de>
2025-08-15 12:29:11 +0200

    fix: null

--- models/matching/rating_similarity_platform_code.sql
+++ models/matching/rating_similarity_platform_code.sql
@@ -8,7 +8,7 @@ WITH platform_codes AS (
     c.globaleid,
     o.osm_id,
     t.assumed_platform AS assumed_platform_code_transit,
-    o.assumed_platform AS assumed_platform_code_osm
+    nullif(o.assumed_platform,'') AS assumed_platform_code_osm
   FROM matching.match_candidates AS c
   JOIN matching.transit_stops AS t
     USING (globaleid)
@@ -17,12 +17,15 @@ WITH platform_codes AS (
 )
 SELECT
   CASE
+    -- Both are null, that's kind of indifferant, but worse than both matching
     WHEN assumed_platform_code_transit IS NULL AND assumed_platform_code_osm IS NULL
     THEN 0.9
     WHEN assumed_platform_code_transit = assumed_platform_code_osm
     THEN 1.0
+    -- If only one is null, it's worse than both null, as either side should matcht
     WHEN assumed_platform_code_transit IS NULL OR assumed_platform_code_osm IS NULL
     THEN 0.85
+    -- if both are not null not equal, they presumably don't match
     ELSE 0.0
   END AS similarity_platform,
   *

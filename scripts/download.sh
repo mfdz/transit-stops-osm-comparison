8ca36003c6192e8d8b76c8eb47b5f907d004868e
Holger Bruch <hb@mfdz.de>
2025-08-15 12:20:46 +0200

    fix: support opendata-oepnv download using cookie

--- scripts/download.sh
+++ scripts/download.sh
@@ -10,9 +10,9 @@ if [ -z "$dest_file" ]; then 1>&2 echo 'missing 2nd argument: dest_file'; exit 1
 ua='User-Agent: transit-stops-osm-comparison'
 etag_file="$dest_file.etag"
 
-if test -e "$$dest_file"
-then zflag="-z '$dest_file'"
+if test -e "$dest_file"
+then zflag="-z $dest_file"
 else zflag=
 fi
 
-curl $zflag -Lf -H "$ua" --compressed -R "$url" '-o' "$dest_file"
+curl -f -H 'Cookie: fe_typo_user=wasverified; path=/' -g $zflag -L -H "$ua" --compressed -R "$url" '-o' "$dest_file"

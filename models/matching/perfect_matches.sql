4952cefe6ca1af43ceeb7ac2385b1f4813234443
Holger Bruch <hb@mfdz.de>
2026-01-06 06:50:58 +0100

    chore: sqlmesh format

--- models/matching/perfect_matches.sql
+++ models/matching/perfect_matches.sql
@@ -2,8 +2,8 @@ MODEL (
   name matching.perfect_matches,
   kind VIEW,
   description "This view comprises all match candidates which are considered unquestionable. These are matches
-    where similiarity is 1.0 (usually because ifopts match perfectly) or the official stop is a station (not a quay), and
-    thus all (ambiguous) match candidates are considered equally."
+      where similiarity is 1.0 (usually because ifopts match perfectly) or the official stop is a station (not a quay), and
+      thus all (ambiguous) match candidates are considered equally."
 );
 
 SELECT
